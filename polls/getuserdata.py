f7d56e14f98c31a872ca6754ddf878ccea97c697
Victoria Kraleva <viktoriakraleva@gmail.com>
2019-06-24 01:21:44 +0200

    modules getrennt

--- polls/getuserdata.py
+++ polls/getuserdata.py
@@ -9,3 +9,28 @@ def getTweets(user_attributes):
           return tweetordered.values()
   else:
     return []
+
+def getDate(tweets):
+  if len(tweets)>0:
+            borndate = tweets[0]['createdat']
+            d0 = datetime.now().date()
+            age = d0 - borndate
+            years = age.days//365
+            if years>0:
+              age = str(years) + " years old"
+            else:
+              age = str(age.days//30 + 1) + " months old"
+  else: 
+            age = "0 days"
+  return age
+
+
+def getFans(user_attributes):
+  user_id = user_attributes[0]['user_id']
+          #followers = Following.objects.filter(user=user_id).values()
+  followers = Following.objects.filter(user_id = user_id).values()
+  fans = []
+  for follower in followers:
+     newuser = User.objects.filter(user_id = follower['follower_id']).values()
+     fans.append(newuser[0])
+  return fans
--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from . forms import NameForm
 from django import forms
 from datetime import datetime
-from . getuserdata import getTweets
+from . getuserdata import getTweets,getDate, getFans
 
 def index(request):
   if request.method == 'GET':
@@ -40,25 +40,8 @@ def users(request, user_screenname):
           user = User.objects.filter(screenname=str(user_screenname))
           user_attributes = user.values()
           tweets = getTweets(user_attributes)
-          if len(tweets)>0:
-            borndate = tweets[0]['createdat']
-            d0 = datetime.now().date()
-            age = d0 - borndate
-            years = age.days//365
-            if years>0:
-              age = str(years) + " years old"
-            else:
-              age = str(age.days//30 + 1) + " months old"
-          else: 
-            age = "0 days"
-          user_id = user_attributes[0]['user_id']
-          #followers = Following.objects.filter(user=user_id).values()
-          followers = Following.objects.filter(user_id = user_id).values()
-          fans = []
-          for follower in followers:
-            newuser = User.objects.filter(user_id = follower['follower_id']).values()
-            print(newuser)
-            fans.append(newuser[0])
+          age = getDate(tweets)
+          fans = getFans(user_attributes)
           return render(request, 'polls/user.html', {
           'tweets': tweets,
           'user': user_attributes[0],
